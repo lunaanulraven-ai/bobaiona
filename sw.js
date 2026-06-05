8116db6c8e12a1c95dbd187557ba930c72ce61c2
Cazé <cazemoises2@gmail.com>
2026-06-05 01:54:13 -0300

    fix: toolbar de edição no bloco hero

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Atualizado automaticamente pelo pre-commit hook a cada commit
-const VERSION = '1780634071372';
+const VERSION = '1780635253451';
 
 self.addEventListener('install', () => self.skipWaiting());
 
