5b8561dde43ac20021516683fe5683073c6e35c4
Cazé <cazemoises2@gmail.com>
2026-06-05 01:34:31 -0300

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Atualizado automaticamente pelo pre-commit hook a cada commit
-const VERSION = '1780633886417';
+const VERSION = '1780634071372';
 
 self.addEventListener('install', () => self.skipWaiting());
 
